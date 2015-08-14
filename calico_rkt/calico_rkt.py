4d1c0f8c1066ee7e7049abbaa9c8ae364678745f
alexhersh <hersh.a@husky.neu.edu>
2015-08-14 14:18:28 -0700

    functions are part of NetworkPlugin class. client is centralized to self

--- calico_rkt/calico_rkt.py
+++ calico_rkt/calico_rkt.py
@@ -25,6 +25,11 @@ print_stderr = functools.partial(print, file=sys.stderr)
 RKT_ORCHESTRATOR = 'rkt'
 INTERFACE_NAME = 'eth0'
 
+ETCD_AUTHORITY_ENV = "ETCD_AUTHORITY"
+if ETCD_AUTHORITY_ENV not in os.environ:
+    os.environ[ETCD_AUTHORITY_ENV] = 'kubernetes-master:6666'
+print("Using ETCD_AUTHORITY=%s" % os.environ[ETCD_AUTHORITY_ENV])
+
 def main():
     print_stderr('Args: ', sys.argv)
     print_stderr('Env: ', os.environ)
@@ -38,7 +43,7 @@ def main():
         print_stderr('No initialization work to perform')
     elif mode == 'ADD':
         print_stderr('Executing Calico pod-creation plugin')
-        add(
+        NetworkPlugin().add(
             pod_id=os.environ['CNI_PODID'],
             netns_path=os.environ['CNI_NETNS'],
             ip='192.168.0.111',
@@ -46,76 +51,80 @@ def main():
     elif mode == 'teardown':
         print_stderr('No pod-deletion work to perform')
 
-def add(pod_id, netns_path, ip):
-    """"Handle rkt pod-add event."""
-    client = datastore.DatastoreClient()
-    print_stderr('Configuring pod %s' % pod_id, file=sys.stderr)
+class NetworkPlugin(object):
 
-    try:
-        endpoint = _create_calico_endpoint(pod_id, ip, netns_path,
-                                           client=client)
-        _create_profile(endpoint=endpoint, profile_name=pod_id, client=client)
-    except CalledProcessError as e:
-        print_stderr('Error code %d creating pod networking: %s\n%s' % (
-            e.returncode, e.output, e))
-        sys.exit(1)
+    def __init__(self):
+        self._datastore_client = DatastoreClient()
 
-def _create_calico_endpoint(pod_id, ip, netns_path, client):
-    """Configure the Calico interface for a pod."""
-    print_stderr('Configuring Calico networking.', file=sys.stderr)
-    endpoint = client.create_endpoint(socket.gethostname(), RKT_ORCHESTRATOR,
-                                      pod_id, [IPAddress(ip)])
-    endpoint.provision_veth(Namespace(netns_path), INTERFACE_NAME)
-    client.set_endpoint(endpoint)
-    print_stderr('Finished configuring network interface', file=sys.stderr)
-    return endpoint
+    def add(pod_id, netns_path, ip):
+        """"Handle rkt pod-add event."""
+        self._datastore_client = datastore.DatastoreClient()
+        print_stderr('Configuring pod %s' % pod_id, file=sys.stderr)
 
-def _create_profile(endpoint, profile_name, client):
-    """
-    Configure the calico profile for a pod.
+        try:
+            endpoint = self._create_calico_endpoint(pod_id, ip, netns_path)
+            self._create_profile(endpoint=endpoint, profile_name=pod_id)
+        except CalledProcessError as e:
+            print_stderr('Error code %d creating pod networking: %s\n%s' % (
+                e.returncode, e.output, e))
+            sys.exit(1)
 
-    Currently assumes one pod with each name.
-    """
-    print_stderr('Configuring Pod Profile: %s' % profile_name)
+    def _create_calico_endpoint(pod_id, ip, netns_path):
+        """Configure the Calico interface for a pod."""
+        print_stderr('Configuring Calico networking.', file=sys.stderr)
+        endpoint = self._datastore_client.create_endpoint(socket.gethostname(), RKT_ORCHESTRATOR,
+                                          pod_id, [IPAddress(ip)])
+        endpoint.provision_veth(Namespace(netns_path), INTERFACE_NAME)
+        self._datastore_client.set_endpoint(endpoint)
+        print_stderr('Finished configuring network interface', file=sys.stderr)
+        return endpoint
 
-    if client.profile_exists(profile_name):
-        print_stderr("Error: Profile with name %s already exists, exiting." % profile_name)
-        sys.exit(1)
+    def _create_profile(endpoint, profile_name):
+        """
+        Configure the calico profile for a pod.
 
-    rules = _create_rules(profile_name)
-    client.create_profile(profile_name, rules)
+        Currently assumes one pod with each name.
+        """
+        print_stderr('Configuring Pod Profile: %s' % profile_name)
 
-    # Also set the profile for the workload.
-    print_stderr('Setting profile %s on endpoint %s' %
-                 (profile_name, endpoint.endpoint_id))
-    client.set_profiles_on_endpoint(
-        profile_name, endpoint_id=endpoint.endpoint_id
-    )
-    print_stderr('Finished configuring profile.')
-    print(json.dumps(
-        {
-            'ip4': {
-                'ip': '192.168.0.111/24'
-            }
-        }))
+        if self._datastore_client.profile_exists(profile_name):
+            print_stderr("Error: Profile with name %s already exists, exiting." % profile_name)
+            sys.exit(1)
 
-def _create_rules(id_):
-    rules_dict = {
-        'id': id_,
-        'inbound_rules': [
+        rules = self._create_rules(profile_name)
+        self._datastore_client.create_profile(profile_name, rules)
+
+        # Also set the profile for the workload.
+        print_stderr('Setting profile %s on endpoint %s' %
+                     (profile_name, endpoint.endpoint_id))
+        self._datastore_client.set_profiles_on_endpoint(
+            profile_name, endpoint_id=endpoint.endpoint_id
+        )
+        print_stderr('Finished configuring profile.')
+        print(json.dumps(
             {
-                'action': 'allow',
-            },
-        ],
-        'outbound_rules': [
-            {
-                'action': 'allow',
-            },
-        ],
-    }
-    rules_json = json.dumps(rules_dict, indent=2)
-    rules = Rules.from_json(rules_json)
-    return rules
+                'ip4': {
+                    'ip': '192.168.0.111/24'
+                }
+            }))
+
+    def _create_rules(id_):
+        rules_dict = {
+            'id': id_,
+            'inbound_rules': [
+                {
+                    'action': 'allow',
+                },
+            ],
+            'outbound_rules': [
+                {
+                    'action': 'allow',
+                },
+            ],
+        }
+        rules_json = json.dumps(rules_dict, indent=2)
+        rules = Rules.from_json(rules_json)
+        return rules
 
 if __name__ == '__main__':
     main()
